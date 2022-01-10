d19b5fd10273382e4dd27f631f42253ccafaeac9
Andrey Efimenko <aa.efimenko@yandex.ru>
2022-01-10 22:38:30 +0300

    fix first task

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,15 +1,18 @@
 // Создаем константу и записываем коллекцию элементов с классом 'modal'
 const elementsModal = document.getElementsByClassName('modal');
-// В коллекции элементов находим первое моадльное окно и добавляем класс, для показа модального окна
+
+// В коллекции элементов находим первое модальное окно и добавляем класс, для показа первого модального окна
 elementsModal.item(0).className = "modal modal_active";
 
 // Создаем константу и записываем коллекцию элементов кнопок-закрытия с классом 'modal__close'
 const elementsButtonsClose = document.getElementsByClassName('modal__close');
 
-// Создаем обрабочик на клик кнопки закрытия для первого модального окна
-elementsButtonsClose.item(0).onclick = function() {
-    elementsModal.item(0).className = "modal";
-    };
+// С помощью цикла создаем УНИВЕРСАЛЬНЫЙ обработчик событий для "крестиков" для закрытия модальных окон
+for (let idx = 0; idx < elementsButtonsClose.length; idx++) {
+    elementsButtonsClose[idx].onclick = function() {
+        this.closest(".modal_active").className = "modal";
+    }
+}
 
 // Создаем обрабочик на клик кнопки "Сделать хорошо" для первого модального окна, открываем второе модальное окно
 elementsButtonsClose.item(1).onclick = function() {
@@ -17,12 +20,7 @@ elementsButtonsClose.item(1).onclick = function() {
     elementsModal.item(1).className = "modal modal_active";
     };
 
-// Создаем обрабочик на клик кнопки закрытия для второго модального окна
-elementsButtonsClose.item(2).onclick = function() {
-    elementsModal.item(1).className = "modal";
-    };
-
-// Создаем константу и записываем коллекцию элементы с классом 'btn_success'
+// Создаем константу и записываем коллекцию элементов с классом 'btn_success'
 const elementsButtonsSuccess = document.getElementsByClassName('btn_success');
 
 // Создаем обрабочик на клик кнопки "Хорошо сделано!" для закрытия второго модального окна
