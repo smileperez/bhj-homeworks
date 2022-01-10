d478df9297081c3ad98f367db1fc79e662c0aed8
Andrey Efimenko <aa.efimenko@yandex.ru>
2022-01-10 23:24:34 +0300

    fix third task

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -22,66 +22,79 @@ function getActiveSlide() {
     return activeSlideNumber;
 }
 
+// Вспомогательная функция для замены слайда
+function changeSlide(nextSlide) {
+    // Убираем текущий активный слайд
+    sliders[getActiveSlide()].className = "slider__item";
+
+    // Выставляем следующий требуемый слайд
+    sliders[nextSlide].className = "slider__item slider__item_active";
+    
+    // Меняем точки управления
+    changeDots(nextSlide);
+}
+
+// Вспомогательная функция для перекраски точек управления
+function changeDots(nextSlide) {
+    // Очищаем белую заливку у каждой точки управления, если она была ранее задана.
+    for (let idx = 0; idx < dots.length; idx++) {
+        if (dots[idx].className === "slider__dot slider__dot_active") {
+            dots[idx].className  = "slider__dot";
+        }
+    }
+
+    // Красим в белый цвет активную точку управления
+    dots[nextSlide].className = "slider__dot slider__dot_active";
+}
+
 // Задаем номер следующего требуемого слайда относительно текущего активного слайда $activeSlideNumber
 let nextSlide;
 
 // Устанавливаем обрабочик собыйтий на элемент управления "Влево"
 buttonLeft.onclick = function() {
+
     // Обозначем следующий требуемый слайд влево
     nextSlide = getActiveSlide() - 1;
+
     // Если следующий слайд -1, то переходим к самому последнему слайду в коллекции
     if (nextSlide < 0) {
         nextSlide = sliders.length - 1;
     }
     console.log(`Текущий слайд: №${nextSlide + 1}`);
 
-    // Убираем текущий активный слайд
-    sliders[getActiveSlide()].className = "slider__item";
-    // Выставляем следующий требуемый слайд
-    sliders[nextSlide].className = "slider__item slider__item_active";
+    // Меняем слайд с помощью вспомогательной функции
+    changeSlide(nextSlide);
 }
 
 
 // Устанавливаем обрабочик собыйтий на элемент управления "Вправо"
 buttonRight.onclick = function() {
+
     // Обозначем следующий требуемый слайд вправо
     nextSlide = getActiveSlide() + 1;
+    
     // Если следующий слайд больше количества слайдов, то возрващаемся к первому слайду в коллекции
     if (nextSlide > sliders.length - 1) {
         nextSlide = 0;
     }
     console.log(`Текущий слайд: №${nextSlide + 1}`);
 
-    // Убираем текущий активный слайд
-    sliders[getActiveSlide()].className = "slider__item";
-    // Выставляем следующий требуемый слайд
-    sliders[nextSlide].className = "slider__item slider__item_active";
+    // Меняем слайд с помощью вспомогательной функции
+    changeSlide(nextSlide);
 }
 
 // ПОВЫШЕННАЯ СЛОЖНОСТЬ
 // Получаем коллецию элементов управления - точек
 const dots = document.getElementsByClassName("slider__dot");
 
-// Циклом устанавливаем орабочик событий по клику на каждкую точку управления
+// Циклом устанавливаем обрабочик событий по клику на каждкую точку управления.
 for (let idx = 0; idx < dots.length; idx++) {
-   
+    // Создаем обрабочик событий по клику на каждую точку
     dots[idx].onclick = function() {
-        // Перед работой основного кода, очищаем белую заливку у каждой точки управления, елси она была ранее задана.
-        for (let i = 0; i < dots.length; i++) {
-            if (dots[i].className === "slider__dot slider__dot_active") {
-                dots[i].className  = "slider__dot";
-            }
-        }
-        
-        // Обозначаем следующий слайд - номер точки по которой кликнули
+        // Обозначаем следующий слайд - номер точки по которой кликнули.
         nextSlide = idx;
-        // Убираем текущий активный слайд
-        sliders[getActiveSlide()].className = "slider__item";
-        // Выставляем следующий требуемый слайд
-        sliders[nextSlide].className = "slider__item slider__item_active";
-        console.log(`Текущий слайд: №${nextSlide + 1}`);
-        
-        // Красим в белый цвет активную точку управления
-        dots[idx].className = "slider__dot slider__dot_active";
+
+        // Меняем слайд с помощью вспомогательной функции
+        changeSlide(nextSlide);
     }
 }
