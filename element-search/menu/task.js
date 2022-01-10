5736bb8ac456a4d87678e315394e6576cd21891b
Andrey Efimenko <aa.efimenko@yandex.ru>
2022-01-11 00:45:30 +0300

    fix second task

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,33 +1,43 @@
-// Получаем коллецицю меню
-const menu = document.getElementsByClassName("menu__link");
+// Получаем коллекцию из вложенных меню <ul>.
+const menuSub = document.getElementsByClassName("menu_sub");
 
-// // Массив только со вложенными меню
-// let arrSubMenu;
-
-// Используя цикл, регистрируем обрабочтик событий на всех менюшках.
-for (let idx = 1; idx < menu.length; idx++) {
-
-    menu[idx].onclick = function() {
-        // Определяем переменную обозначающую вложенное меню.
-        let menuSub = menu[idx+1].closest(".menu_sub");
-
-        // Сначала закрываем все другие открытые вложенные меню
-        for (let i = 0; i < menu.length; i++) {
-            if (menu[i].closest(".menu_active") != null) {
-                menu[i].closest(".menu_sub").className = "menu menu_sub";
+// Находим ссылки <a> у которых существует подменю.
+// Создадим массив, для записи в него всех найденных ссылок <a> у которых существует подменю.
+let menuSubLinks = [];
+// Переберем все вложенные меню <ul>, найдем их родителей <li class="menu__item"> и у этих родителей найдем ссылки <a>
+for (let idx = 0; idx < menuSub.length; idx++) {
+    // Находим у вложенных меню родителя, а у родителя находим ссылки. Пихаем в массив.
+    menuSubLinks.push(menuSub[idx].closest(".menu__item").querySelector(".menu__link"));
+    
+    // Ставим обработчик событий на найденные ссылки.
+    menuSubLinks[idx].onclick = function() {
+        
+        let openedMenuSub;
+        // Пытаемся найти уже открытое меню и закрыть его
+        for (let i = 0; i < menuSubLinks.length; i++) {
+            // Если находим открытое меню - закрываем его!
+            if (menuSub[i].className.includes("menu_active")) {
+                // Закрываем!
+                menuSub[i].className = "menu menu_sub";
+                console.log(`Закрыли меню ${i}`);
+                // Зафиксировали ранее закрытое меню
+                openedMenuSub = i;
             }
         }
 
-        // Если вложенное меню нашлось без класса menu_active, то добавляем класс menu_active (включаем меню при клике).
-        if (menuSub.className === "menu menu_sub") {
-            menuSub.className = "menu menu_sub menu_active";
-
-        // Если вложенное меню нашлось с классом menu_active, то убираем класс menu_active (выключаем меню при клике).
+        // Проверяем, а какое именно меню мы открываем?
+        if (idx != openedMenuSub) {
+            // Открываем кликнутое меню
+            menuSub[idx].className = "menu menu_sub menu_active";
+            console.log(`///---///`);
+            console.log(`Открыли меню ${idx}`);
+            
+            // Запрещаем обновление страницы
+            return false;
         } else {
-            menuSub.className = "menu menu_sub";
+            // Запрещаем обновление страницы
+            return false
         }
 
-        // Запрещаем переход по ссылке тех меню, что имеют вложенное меню.
-        return false
     }
 }
