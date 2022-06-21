45bc7f434572777773adf0e201989a20b4e7c015
smileperez <aa.efimenko@efimen.ru>
2022-06-21 22:48:42 +0300

    fix 1 task

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -8,24 +8,48 @@ for (let idx = 0; idx < hasToolkit.length; idx++) {
     tooltipElem.className = 'tooltip';
     tooltipElem.innerHTML = hasToolkit[idx].getAttribute('title');;
     
-   
-
     // Добавляем созданные подскази в разметку.
     hasToolkit[idx].insertAdjacentElement('afterEnd', tooltipElem);
 }
 
+// Найдем все созданные подсказки
 const toolkits = document.querySelectorAll('.tooltip');
 console.log(toolkits);
+
 
 
 // Триггеруем добавление/убавление класса видимости подсказки на каждую подсказку
 for (let idx = 0; idx < hasToolkit.length; idx++) {
 
+    // let activeFlag = 0;
     // Устанавливаем событие на клик
     hasToolkit[idx].onclick = function() {
+
+        // if (activeFlag == 1) {
+        //     toolkits[idx].classList.toggle("tooltip_active");
+        //     activeFlag = 0;
+        // }
         
         // Саначала скрываем все подсказки при клике на любую подсказку.
-        closeToolkits();
+        // closeToolkits();
+        // toolkits[idx].classList.toggle("tooltip_active");
+
+        
+       
+
+        // for (let jdx = 0; jdx < hasToolkit.length; jdx++) {
+        //     if (toolkits.find(findClass(jdx))) {
+        //         console.log(`нашли класс в ${jdx}`);
+        //         return false;
+        //     } else {
+        //         console.log(`нигде не нашли такой класс`);
+        //         return false;
+        //     }
+        // }
+
+        // function findClass(jdx) {
+        //     toolkits[jdx].classList.contains("tooltip_active");
+        // }
 
         // Позиционируем подсказку, для этого находим координаты куда установить подсказку.
         let coords = hasToolkit[idx].getBoundingClientRect();
@@ -37,8 +61,23 @@ for (let idx = 0; idx < hasToolkit.length; idx++) {
         toolkits[idx].style.top = top + 'px';
         
         // Включаем активную подсказку при клике
-        toolkits[idx].classList.toggle("tooltip_active");
+        // toolkits[idx].classList.toggle("tooltip_active");
+
+        if (toolkits[idx].classList.contains("tooltip_active")) {
+            for (let jdx = 0; jdx < hasToolkit.length; jdx++) {
+                toolkits[jdx].classList.remove("tooltip_active");
+            }
+        } else {
+            for (let jdx = 0; jdx < hasToolkit.length; jdx++) {
+                toolkits[jdx].classList.remove("tooltip_active");
+            }
+            toolkits[idx].classList.toggle("tooltip_active");
+        }
+
         return false;
+
+
+       
     }
 }
 
